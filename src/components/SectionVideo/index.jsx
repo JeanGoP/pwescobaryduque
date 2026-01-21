91ea77a0571987cbcc7f13cb660968a6dd6ed411
Jose David Malambo <93331068+JoseDavid06@users.noreply.github.com>
2026-01-21 16:22:03 -0500

    Update index.jsx

--- src/components/SectionVideo/index.jsx
+++ src/components/SectionVideo/index.jsx
@@ -4,7 +4,7 @@ export default function TelemedicinaHeroSection() {
   return (
     <section className="tmh-section">
       <div className="tmh-video-wrapper">
-        <video src="/video.mp4" autoPlay muted loop playsInline />
+        <video src="https://desarrollosidecil.s3.amazonaws.com/ESCOBARDUQUE_IMGS/video.mp4" autoPlay muted loop playsInline />
       </div>
     </section>
   );
